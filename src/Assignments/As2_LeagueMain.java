e4c02ff2ceba56fdf6a763d7717fefbd744fa117
apshort <andrea.s.982473@gmail.com>
2024-11-27 15:24:53 -0700

    second assignment commit 1

---
+++ src/Assignments/As2_LeagueMain.java
@@ -0,0 +1,15 @@
+package Assignments;
+
+public class As2_LeagueMain {
+
+    public static void run(){
+
+        //pink, red, orange, yellow = division ROY
+        //lime, green, cyan = division G
+        //aqua, blue, purple = division BIV
+        //40 total games
+        //red avg =
+
+    }//run
+
+}
---
+++ src/Assignments/As2_Team.java
@@ -0,0 +1,22 @@
+package Assignments;
+
+public class As2_Team {
+
+    private String nickname;
+    private String country;
+    private String division;
+    private int numWins;
+    private int numLasts;
+    private double avgPlacement;
+
+    public As2_Team (String n, String c, String d, int w, int l, double a){
+        nickname = n;
+        country = c;
+        division = d;
+        numWins = w;
+        numLasts = l;
+        avgPlacement = a;
+
+    }
+
+}
